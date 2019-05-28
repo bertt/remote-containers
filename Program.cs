ba2fdcb355e1720ce4cb5769c8995d407f206ce1
bert <btemme@hotmail.com>
2019-05-28 20:13:17 +0000

    initial

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace remote_containers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press a key");
+            var key = Console.ReadKey();
+            Console.WriteLine("Hello World! " + key.KeyChar);
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
